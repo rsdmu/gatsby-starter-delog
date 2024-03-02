7ae29d3c7ded8752bc132e9563e50f5c5589df6e
rsdmu <rashid.ahmad.mushkani@umontreal.ca>
2024-03-02 17:20:07 -0500

    commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,8 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
-import Navigation from "../components/navigation"
-import 'prismjs/themes/prism-okaidia.css';
+import Navigation from "./navigation" // Assuming Navigation is also in the same components directory
+import SEO from "./seo" // Importing SEO from the same directory
+import 'prismjs/themes/prism-okaidia.css'
 
 export default ({ children }) => {
   const data = useStaticQuery(
@@ -15,8 +16,14 @@ export default ({ children }) => {
       }
     `
   )
+
   return (
     <div className="site-wrapper">
+      <SEO 
+        title={data.site.siteMetadata.title} 
+        description="Your site description here" // Add your site description
+        metaTag="l3J0y3XD8D3n9javX6yPcBrEYB9SXnICRguNy3C5JHA" // Your specific Google verification content
+      />
       <header className="site-header">
         <div className="site-title">
           <Link to="/">{data.site.siteMetadata.title}</Link>
@@ -30,5 +37,3 @@ export default ({ children }) => {
     </div>
   )
 }
-
-
